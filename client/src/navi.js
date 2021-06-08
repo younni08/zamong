6efaefe862805234af8cc358ee3c1cd97eea6982
younni08 <younni08@gmail.com>
2021-06-08 18:11:44 +0900

    check

---
+++ client/src/navi.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Navi = () => {
+    return (
+        <div>
+            <span>이루자몽</span>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Navi;
--- client/src/root.js
+++ client/src/root.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React from "react";
+import Navi from "./navi"
+
 
 const Root = () => {
     return (
         <div>
-
+            <Navi />
+            <div className="tempbody">
+                <div>
+                    <span>use</span>
+                    <span>less</span>
+                </div>
+                <span>let's go</span>
+            </div>
         </div>
     )
 }
